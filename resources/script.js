10a138632fbefba8292d76256be89ec58de5601c
kathleenmoriarty <149009823+kathleenmoriarty@users.noreply.github.com>
2025-03-24 14:29:40 -0700

    Add completed About section with script tags

---
+++ resources/script.js
@@ -0,0 +1,13 @@
+const tablinks = document.getElementsByClassName('tab-links');
+
+const tabcontents = document.getElementsByClassName('tab-contents');
+
+function opentab (tabName) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents) {
+        tabcontents.classList.remove("active-tab";)
+    }
+}
